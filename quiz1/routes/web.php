247214c02a9655ae4ccdfe0fc7d21770c298b1cd
Patricia Bernadette Aspillaga <144974708+TriciaBAA@users.noreply.github.com>
2025-03-07 03:54:23 +0000

    done up to step 7

---
+++ quiz1/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hello/{name}', function($name) {
+
+    return 'hi ' . $name; // It will say "hi" followed by whatever name you typed (like "hi John")
+
+});
